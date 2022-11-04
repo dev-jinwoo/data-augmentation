2c7b41332aa5ff65db19a348295844e136ddb39a
chakam1307 <az13077@gmail.com>
2022-11-04 12:21:30 +0900

    feat : Add resize_image function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PIL import Image
-
+import cv2
 
 def crop_image(img, bbox):
     """
@@ -21,6 +21,19 @@ def rotate_image(img, angle):
     rotated_image = img.rotate(angle)
 
     return rotated_image
+
+def resize_image(src_img, tgt_size=512):
+    if src_img.shape[0] == tgt_size and src_img.shape[1] == tgt_size:
+        return src_img
+
+    if src_img.shape[0] > src_img.shape[1]:
+        resize_ratio = tgt_size / src_img.shape[0]
+    else:
+        resize_ratio = tgt_size / src_img.shape[1]
+
+    tgt_img = cv2.resize(src_img, dsize=(0, 0), fx=resize_ratio, fy=resize_ratio, interpolation=cv2.INTER_AREA)
+
+    return tgt_img
 
 
 if __name__ == "__main__":
