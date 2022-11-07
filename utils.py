eaae3eeb02baf96486f1b195450e6ed775f7ee3e
dev-jinwoo <jinwoohong.dev@gmail.com>
2022-11-07 21:51:38 +0900

    feat : Add occlude image function

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from PIL import Image
-import cv2
+import random
+import numpy as np
 
 
 def crop_image(img, bbox):
@@ -44,6 +45,36 @@ def resize_image(src_img, tgt_size=512):
     resize_height = int(height * resize_ratio)
 
     tgt_img = src_img.resize((resize_width, resize_height))
+
+    return tgt_img
+
+
+def occlude_image(src_img, prob=2):
+    """
+    :param src_img: PIL image
+    :param prob: augmentation probability
+          (prob = 1 : 100%, prob = 2 : 50%, prob = 3 :  33%, ...)
+    :return: occluded image
+    """
+    aug_prob = random.uniform(0, prob)
+
+    np_img = np.array(src_img)
+
+    # RGB Color Codes Chart : https://www.rapidtables.com/web/color/RGB_Color.html
+    color = (200 + random.randint(-30, 30), 150 + random.randint(-30, 30), 50 + random.randint(-30, 30))
+
+    width, height = np_img.shape[0], np_img.shape[1]
+
+    if 0 < aug_prob < 0.25:
+        np_img[:, :height // 2, :] = color
+    elif 0.25 < aug_prob < 0.5:
+        np_img[:, height // 2:, :] = color
+    elif 0.5 < aug_prob < 0.75:
+        np_img[width // 2:, :, :] = color
+    elif 0.75 < aug_prob < 1:
+        np_img[:width // 2, :, :] = color
+
+    tgt_img = Image.fromarray(np_img)
 
     return tgt_img
 
